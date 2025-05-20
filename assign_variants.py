a6dba0f2a354ca4a504c8603663df43cf344376a
taneshsin <tanesh.singhal459@gmail.com>
2025-05-20 01:14:11 -0400

    Update assign_variants.py

--- assign_variants.py
+++ assign_variants.py
@@ -2,12 +2,12 @@ import pandas as pd
 import random
 
 # Load the real churn dataset
-df = pd.read_csv("data/customer_churn_dataset-testing-master.csv")
+df = pd.read_csv("data/churn.csv")
 
 # Assign A or B randomly
 df["variant"] = [random.choice(["A", "B"]) for _ in range(len(df))]
 
 # Save back to same file
-df.to_csv("data/customer_churn_dataset-testing-master.csv", index=False)
+df.to_csv("data/churn.csv", index=False)
 
 print("✅ A/B variants assigned and saved to churn dataset.")
